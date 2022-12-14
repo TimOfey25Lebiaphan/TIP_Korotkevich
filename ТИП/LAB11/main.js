f3c1762d378e98d5362881f6c7707c8d3d72ef68
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2022-12-14 13:03:52 +0300

    1

--- ТИП/LAB11/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $("#my_links li a").css('color', 'magenta');
-  $("#my_links li a[href^='documents']").css({'color': 'orange'});
-
-  $("td").css({'border': '3px solid gray', 'padding': '10px','font-size': '20pt'});
-  $("table").css({'border-collapse': 'collapse'});
-  $("table#moto_table tr:nth-child(even) > td:last-child").css({'background-color': 'green','color': 'white'});
-});
--- ТИП/LAB2/task2.js
+++
@@ -1,6 +0,0 @@
-function Task2() {
-  let radius = prompt('Введите длину первого катета ');
-  let bSide = prompt('Введите длину второго катета ');
-  let cSide = Math.sqrt(Math.pow(aSide, 2) + Math.pow(bSide, 2));
-  alert('Длина гипотенузы C: ' + cSide);
-};
